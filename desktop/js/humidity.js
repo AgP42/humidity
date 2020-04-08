eb868af0cd7cfe3e8d670f1b29983f6e4585be92
Agathe Pinel <agp.com@mailbox.org>
2020-04-08 20:48:46 +0200

    page config ok et fonctionnelle

--- desktop/js/humidity.js
+++ desktop/js/humidity.js
@@ -15,6 +15,134 @@
  * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
  */
 
+ // permet de reorganiser les elements de la div en les cliquant/deplacant
+$("#div_action_on").sortable({axis: "y", cursor: "move", items: ".action_on", placeholder: "ui-state-highlight", tolerance: "intersect", forcePlaceholderSize: true});
+$("#div_action_off").sortable({axis: "y", cursor: "move", items: ".action_off", placeholder: "ui-state-highlight", tolerance: "intersect", forcePlaceholderSize: true});
+$("#div_action_alert").sortable({axis: "y", cursor: "move", items: ".action_alert", placeholder: "ui-state-highlight", tolerance: "intersect", forcePlaceholderSize: true});
+
+
+// tous les boutons d'action
+$('.addAction').off('click').on('click', function () {
+  addAction({}, $(this).attr('data-type'));
+});
+
+
+// tous les - qui permettent de supprimer la ligne
+$("body").off('click','.bt_removeAction').on('click','.bt_removeAction',function () {
+  var type = $(this).attr('data-type');
+  $(this).closest('.' + type).remove();
+});
+
+
+// permet d'afficher la liste des cmd Jeedom pour choisir sa commande de type "info" (pas les actions donc)
+$("body").off('click','.listCmdInfo').on('click','.listCmdInfo', function () {
+  var el = $(this).closest('.form-group').find('.eqLogicAttr');
+  jeedom.cmd.getSelectModal({cmd: {type: 'info'}}, function (result) {
+    if (el.attr('data-concat') == 1) {
+      el.atCaret('insert', result.human);
+    } else {
+      el.value(result.human);
+    }
+  });
+});
+
+// permet d'afficher la liste des cmd Jeedom pour choisir sa commande de type "action"
+$("body").off('click','.listCmdAction').on('click','.listCmdAction', function () {
+  var type = $(this).attr('data-type');
+  var el = $(this).closest('.' + type).find('.expressionAttr[data-l1key=cmd]');
+  jeedom.cmd.getSelectModal({cmd: {type: 'action'}}, function (result) {
+    el.value(result.human);
+    jeedom.cmd.displayActionOption(el.value(), '', function (html) {
+      el.closest('.' + type).find('.actionOptions').html(html);
+    });
+
+  });
+});
+
+// copier/coller du core (cmd.configure.php), permet de choisir la liste des actions (scenario, attendre, ...)
+$("body").undelegate(".listAction", 'click').delegate(".listAction", 'click', function () {
+  var type = $(this).attr('data-type');
+  var el = $(this).closest('.' + type).find('.expressionAttr[data-l1key=cmd]');
+  jeedom.getSelectActionModal({}, function (result) {
+    el.value(result.human);
+    jeedom.cmd.displayActionOption(el.value(), '', function (html) {
+      el.closest('.' + type).find('.actionOptions').html(html);
+      taAutosize();
+    });
+  });
+});
+
+//////////////// Les fonctions ACTIONS /////////////////////////////////
+
+// fonction générique pour ajouter chaque ligne d'action.
+// _type peut etre 'action_on', 'action_off', 'action_alert'
+function addAction(_action, _type) {
+  var div = '<div class="' + _type + '">';
+    div += '<div class="form-group ">';
+
+      div += '<label class="col-sm-1 control-label">Action</label>';
+      div += '<div class="col-sm-4">';
+        div += '<div class="input-group">';
+          div += '<span class="input-group-btn">';
+            div += '<a class="btn btn-default bt_removeAction roundedLeft" data-type="' + _type + '"><i class="fas fa-minus-circle"></i></a>';
+          div += '</span>';
+          div += '<input class="expressionAttr form-control cmdAction" data-l1key="cmd" data-type="' + _type + '" />';
+          div += '<span class="input-group-btn">';
+            div += '<a class="btn btn-default listAction" data-type="' + _type + '" title="{{Sélectionner un mot-clé}}"><i class="fa fa-tasks"></i></a>';
+            div += '<a class="btn btn-default listCmdAction roundedRight" data-type="' + _type + '" title="{{Sélectionner une commande}}"><i class="fas fa-list-alt"></i></a>';
+          div += '</span>';
+        div += '</div>';
+      div += '</div>';
+
+      div += '<div class="col-sm-7 actionOptions">';
+        div += jeedom.cmd.displayActionOption(init(_action.cmd, ''), _action.options);
+      div += '</div>';
+
+    div += '</div>';
+  div += '</div>';
+
+  $('#div_' + _type).append(div);
+  $('#div_' + _type + ' .' + _type + '').last().setValues(_action, '.expressionAttr');
+}
+
+// Fct core permettant de sauvegarder
+function saveEqLogic(_eqLogic) {
+  if (!isset(_eqLogic.configuration)) {
+    _eqLogic.configuration = {};
+  }
+  _eqLogic.configuration.action_on = $('#div_action_on .action_on').getValues('.expressionAttr');
+  _eqLogic.configuration.action_off = $('#div_action_off .action_off').getValues('.expressionAttr');
+  _eqLogic.configuration.action_alert = $('#div_action_alert .action_alert').getValues('.expressionAttr');
+
+  return _eqLogic;
+}
+
+// fct core permettant de restituer les cmd declarées
+function printEqLogic(_eqLogic) {
+
+  $('#div_action_on').empty();
+  $('#div_action_off').empty();
+  $('#div_action_alert').empty();
+
+  if (isset(_eqLogic.configuration)) {
+    if (isset(_eqLogic.configuration.action_on)) {
+      for (var i in _eqLogic.configuration.action_on) {
+        addAction(_eqLogic.configuration.action_on[i], 'action_on');
+      }
+    }
+    if (isset(_eqLogic.configuration.action_off)) {
+      for (var i in _eqLogic.configuration.action_off) {
+        addAction(_eqLogic.configuration.action_off[i], 'action_off');
+      }
+    }
+    if (isset(_eqLogic.configuration.action_alert)) {
+      for (var i in _eqLogic.configuration.action_alert) {
+        addAction(_eqLogic.configuration.action_alert[i], 'action_alert');
+      }
+    }
+  }
+}
+
 
 $("#table_cmd").sortable({axis: "y", cursor: "move", items: ".cmd", placeholder: "ui-state-highlight", tolerance: "intersect", forcePlaceholderSize: true});
 /*
--- desktop/php/humidity.php
+++ desktop/php/humidity.php
@@ -9,114 +9,217 @@ $eqLogics = eqLogic::byType($plugin->getId());
 
 <div class="row row-overflow">
    <div class="col-xs-12 eqLogicThumbnailDisplay">
-  <legend><i class="fas fa-cog"></i>  {{Gestion}}</legend>
-  <div class="eqLogicThumbnailContainer">
-      <div class="cursor eqLogicAction logoPrimary" data-action="add">
-        <i class="fas fa-plus-circle"></i>
-        <br>
-        <span>{{Ajouter}}</span>
+    <legend><i class="fas fa-cog"></i>  {{Gestion}}</legend>
+    <div class="eqLogicThumbnailContainer">
+        <div class="cursor eqLogicAction logoPrimary" data-action="add">
+          <i class="fas fa-plus-circle"></i>
+          <br>
+          <span>{{Ajouter}}</span>
+      </div>
+        <div class="cursor eqLogicAction logoSecondary" data-action="gotoPluginConf">
+        <i class="fas fa-wrench"></i>
+      <br>
+      <span>{{Configuration}}</span>
     </div>
-      <div class="cursor eqLogicAction logoSecondary" data-action="gotoPluginConf">
-      <i class="fas fa-wrench"></i>
-    <br>
-    <span>{{Configuration}}</span>
+    </div>
+    <legend><i class="fas fa-tint"></i> {{Mes équipements}}</legend>
+  	   <input class="form-control" placeholder="{{Rechercher}}" id="in_searchEqlogic" />
+    <div class="eqLogicThumbnailContainer">
+        <?php
+    foreach ($eqLogics as $eqLogic) {
+    	$opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard';
+    	echo '<div class="eqLogicDisplayCard cursor '.$opacity.'" data-eqLogic_id="' . $eqLogic->getId() . '">';
+    	echo '<img src="' . $plugin->getPathImgIcon() . '"/>';
+    	echo '<br>';
+    	echo '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
+    	echo '</div>';
+    }
+    ?>
+    </div>
   </div>
-  </div>
-  <legend><i class="fas fa-table"></i> {{Mes humidity}}</legend>
-	   <input class="form-control" placeholder="{{Rechercher}}" id="in_searchEqlogic" />
-<div class="eqLogicThumbnailContainer">
-    <?php
-foreach ($eqLogics as $eqLogic) {
-	$opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard';
-	echo '<div class="eqLogicDisplayCard cursor '.$opacity.'" data-eqLogic_id="' . $eqLogic->getId() . '">';
-	echo '<img src="' . $plugin->getPathImgIcon() . '"/>';
-	echo '<br>';
-	echo '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
-	echo '</div>';
-}
-?>
-</div>
-</div>
 
-<div class="col-xs-12 eqLogic" style="display: none;">
+  <div class="col-xs-12 eqLogic" style="display: none;">
 		<div class="input-group pull-right" style="display:inline-flex">
 			<span class="input-group-btn">
 				<a class="btn btn-default btn-sm eqLogicAction roundedLeft" data-action="configure"><i class="fa fa-cogs"></i> {{Configuration avancée}}</a><a class="btn btn-default btn-sm eqLogicAction" data-action="copy"><i class="fas fa-copy"></i> {{Dupliquer}}</a><a class="btn btn-sm btn-success eqLogicAction" data-action="save"><i class="fas fa-check-circle"></i> {{Sauvegarder}}</a><a class="btn btn-danger btn-sm eqLogicAction roundedRight" data-action="remove"><i class="fas fa-minus-circle"></i> {{Supprimer}}</a>
 			</span>
 		</div>
-  <ul class="nav nav-tabs" role="tablist">
-    <li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
-    <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fas fa-tachometer-alt"></i> {{Equipement}}</a></li>
-    <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
-  </ul>
-  <div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
-    <div role="tabpanel" class="tab-pane active" id="eqlogictab">
+    <ul class="nav nav-tabs" role="tablist">
+      <li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
+      <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fas fa-tachometer-alt"></i> {{Equipement}}</a></li>
+
+      <li role="presentation"><a href="#actionstab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fas fa-hand-point-right"></i> {{Actions}}</a></li>
+
+      <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
+
+    </ul>
+    <div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
+
+      <!-- TAB equipement -->
+      <div role="tabpanel" class="tab-pane active" id="eqlogictab">
       <br/>
-    <form class="form-horizontal">
-        <fieldset>
+        <form class="form-horizontal">
+          <fieldset>
+          <legend><i class="fas fa-tachometer-alt"></i> {{Général}}</legend>
             <div class="form-group">
-                <label class="col-sm-3 control-label">{{Nom de l'équipement template}}</label>
-                <div class="col-sm-3">
-                    <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-                    <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement template}}"/>
-                </div>
+              <label class="col-sm-3 control-label">{{Nom de l'équipement}}</label>
+              <div class="col-sm-3">
+                <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+                <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement}}"/>
+              </div>
             </div>
             <div class="form-group">
-                <label class="col-sm-3 control-label" >{{Objet parent}}</label>
-                <div class="col-sm-3">
-                    <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
-                        <option value="">{{Aucun}}</option>
-                        <?php
-foreach (jeeObject::all() as $object) {
-	echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
-}
-?>
-                   </select>
-               </div>
-           </div>
-	   <div class="form-group">
-                <label class="col-sm-3 control-label">{{Catégorie}}</label>
-                <div class="col-sm-9">
-                 <?php
-                    foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
+            <label class="col-sm-3 control-label" >{{Objet parent}}</label>
+              <div class="col-sm-3">
+                <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
+                  <option value="">{{Aucun}}</option>
+                  <?php
+                    foreach (jeeObject::all() as $object) {
+                    	echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
+                    }
+                    ?>
+                </select>
+              </div>
+            </div>
+
+  	        <div class="form-group">
+            <label class="col-sm-3 control-label">{{Catégorie}}</label>
+              <div class="col-sm-9">
+                <?php
+                  foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
                     echo '<label class="checkbox-inline">';
                     echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
                     echo '</label>';
                     }
                   ?>
-               </div>
-           </div>
-	<div class="form-group">
-		<label class="col-sm-3 control-label"></label>
-		<div class="col-sm-9">
-			<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
-			<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
-		</div>
-	</div>
-       <div class="form-group">
-        <label class="col-sm-3 control-label">{{template param 1}}</label>
-        <div class="col-sm-3">
-            <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="city" placeholder="param1"/>
-        </div>
-    </div>
-</fieldset>
-</form>
-</div>
-      <div role="tabpanel" class="tab-pane" id="commandtab">
-<a class="btn btn-success btn-sm cmdAction pull-right" data-action="add" style="margin-top:5px;"><i class="fa fa-plus-circle"></i> {{Commandes}}</a><br/><br/>
-<table id="table_cmd" class="table table-bordered table-condensed">
-    <thead>
-        <tr>
-            <th>{{Nom}}</th><th>{{Type}}</th><th>{{Action}}</th>
-        </tr>
-    </thead>
-    <tbody>
-    </tbody>
-</table>
-</div>
-</div>
+              </div>
+            </div>
 
-</div>
+          	<div class="form-group">
+          		<label class="col-sm-3 control-label"></label>
+          		<div class="col-sm-9">
+          			<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
+          			<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
+          		</div>
+          	</div>
+
+          </fieldset>
+        </form>
+
+        <form class="form-horizontal">
+          <fieldset>
+            <legend><i class="fas fa-tint"></i> {{Configuration}}</legend>
+
+            <div class="form-group">
+              <label class="col-sm-2 control-label">{{Type}}
+              </label>
+              <div class="col-sm-2">
+                <select class="eqLogicAttr form-control tooltips" data-l1key="configuration" data-l2key="humidity_type">
+                  <option value="humid" selected>Humidificateur</option>
+                  <option value="deshumid">Déshumidificateur</option>
+                </select>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label class="col-sm-2 control-label">{{Consigne}}<sup><i class="fas fa-question-circle tooltips" title="{{}}"></i></sup></label>
+              <div class="col-sm-4">
+                <div class="input-group">
+                  <input type="text" class="eqLogicAttr form-control tooltips roundedLeft" data-l1key="configuration" data-l2key="target_humidity"/>
+                  <span class="input-group-btn">
+                    <a class="btn btn-default listCmdInfo roundedRight"><i class="fa fa-list-alt"></i></a>
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label class="col-sm-2 control-label">{{Sonde humidité}}<sup><i class="fas fa-question-circle tooltips" title="{{}}"></i></sup></label>
+              <div class="col-sm-4">
+                <div class="input-group">
+                  <input type="text" class="eqLogicAttr form-control tooltips roundedLeft" data-l1key="configuration" data-l2key="sensor_humidity"/>
+                  <span class="input-group-btn">
+                    <a class="btn btn-default listCmdInfo roundedRight"><i class="fa fa-list-alt"></i></a>
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label class="col-sm-2 control-label">{{Consommation électrique}}<sup><i class="fas fa-question-circle tooltips" title="{{}}"></i></sup></label>
+              <div class="col-sm-4">
+                <div class="input-group">
+                  <input type="text" class="eqLogicAttr form-control tooltips roundedLeft" data-l1key="configuration" data-l2key="conso"/>
+                  <span class="input-group-btn">
+                    <a class="btn btn-default listCmdInfo roundedRight"><i class="fa fa-list-alt"></i></a>
+                  </span>
+                </div>
+              </div>
+            </div>
+
+          </fieldset>
+        </form>
+
+      </div>
+
+      <!-- TAB actions -->
+      <div class="tab-pane" id="actionstab">
+
+        <br/>
+        <div class="alert alert-info">
+          {{}}
+        </div>
+
+        <form class="form-horizontal">
+          <fieldset>
+            <legend><i class="fas fa-toggle-on"></i> {{Pour lancer, je dois ?}} <sup><i class="fas fa-question-circle tooltips" title="{{}}"></i></sup>
+              <a class="btn btn-success btn-sm addAction" data-type="action_on" style="margin:5px;"><i class="fas fa-plus-circle"></i> {{Ajouter une action}}</a>
+            </legend>
+            <div id="div_action_on"></div>
+
+          </fieldset>
+        </form>
+
+        <form class="form-horizontal">
+          <fieldset>
+            <legend><i class="fas fa-toggle-off"></i> {{Pour arrêter, je dois ?}} <sup><i class="fas fa-question-circle tooltips" title="{{}}"></i></sup>
+              <a class="btn btn-success btn-sm addAction" data-type="action_off" style="margin:5px;"><i class="fas fa-plus-circle"></i> {{Ajouter une action}}</a>
+            </legend>
+            <div id="div_action_off"></div>
+
+          </fieldset>
+        </form>
+
+        <form class="form-horizontal">
+          <fieldset>
+            <legend><i class="fas fa-child"></i> {{Pour alerter, je dois ?}} <sup><i class="fas fa-question-circle tooltips" title="{{Actions réalisées lorsque le plugin commande l'appareil mais la prise ne remonte pas de consommation. Celà signifie probablement que le réservoir de l'humidificateur est vide, ou celui du déshumidificateur est plein. }}"></i></sup>
+              <a class="btn btn-success btn-sm addAction" data-type="action_alert" style="margin:5px;"><i class="fas fa-plus-circle"></i> {{Ajouter une action}}</a>
+            </legend>
+            <div id="div_action_alert"></div>
+
+          </fieldset>
+        </form>
+
+        <br>
+
+      </div>
+
+      <!-- TAB commandes -->
+      <div role="tabpanel" class="tab-pane" id="commandtab">
+        <a class="btn btn-success btn-sm cmdAction pull-right" data-action="add" style="margin-top:5px;"><i class="fa fa-plus-circle"></i> {{Commandes}}</a><br/><br/>
+        <table id="table_cmd" class="table table-bordered table-condensed">
+          <thead>
+            <tr>
+              <th>{{Nom}}</th><th>{{Type}}</th><th>{{Action}}</th>
+            </tr>
+          </thead>
+          <tbody>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+  </div>
 </div>
 
 <?php include_file('desktop', 'humidity', 'js', 'humidity');?>
